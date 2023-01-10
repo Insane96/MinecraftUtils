dd4fb385d9332f0f389f0cb464dbc399027a4c86
Insane96 <delvillano.alberto@gmail.com>
2023-01-10 20:30:26 +0100

    Now downloads the forge installer instead of having to select the forge folder

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -29,43 +29,34 @@
         private void InitializeComponent()
         {
             this.btnSelectModPackFolder = new System.Windows.Forms.Button();
-            this.btnSelectForgeServerFolder = new System.Windows.Forms.Button();
             this.chkFolders = new System.Windows.Forms.CheckedListBox();
             this.btnGenerateServerZip = new System.Windows.Forms.Button();
             this.txtLog = new System.Windows.Forms.TextBox();
+            this.cmbGameVersions = new System.Windows.Forms.ComboBox();
+            this.cmbForgeVersion = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // btnSelectModPackFolder
             // 
-            this.btnSelectModPackFolder.Location = new System.Drawing.Point(253, 12);
+            this.btnSelectModPackFolder.Location = new System.Drawing.Point(12, 12);
             this.btnSelectModPackFolder.Name = "btnSelectModPackFolder";
-            this.btnSelectModPackFolder.Size = new System.Drawing.Size(220, 23);
+            this.btnSelectModPackFolder.Size = new System.Drawing.Size(461, 23);
             this.btnSelectModPackFolder.TabIndex = 0;
             this.btnSelectModPackFolder.Text = "Select ModPack Folder";
             this.btnSelectModPackFolder.UseVisualStyleBackColor = true;
             this.btnSelectModPackFolder.Click += new System.EventHandler(this.btnSelectModPackFolder_Click);
             // 
-            // btnSelectForgeServerFolder
-            // 
-            this.btnSelectForgeServerFolder.Location = new System.Drawing.Point(12, 12);
-            this.btnSelectForgeServerFolder.Name = "btnSelectForgeServerFolder";
-            this.btnSelectForgeServerFolder.Size = new System.Drawing.Size(220, 23);
-            this.btnSelectForgeServerFolder.TabIndex = 1;
-            this.btnSelectForgeServerFolder.Text = "Select Forge Server Folder";
-            this.btnSelectForgeServerFolder.UseVisualStyleBackColor = true;
-            this.btnSelectForgeServerFolder.Click += new System.EventHandler(this.btnSelectForgeServerFolder_Click);
-            // 
             // chkFolders
             // 
             this.chkFolders.FormattingEnabled = true;
-            this.chkFolders.Location = new System.Drawing.Point(12, 41);
+            this.chkFolders.Location = new System.Drawing.Point(12, 83);
             this.chkFolders.Name = "chkFolders";
             this.chkFolders.Size = new System.Drawing.Size(461, 310);
             this.chkFolders.TabIndex = 2;
             // 
             // btnGenerateServerZip
             // 
-            this.btnGenerateServerZip.Location = new System.Drawing.Point(12, 514);
+            this.btnGenerateServerZip.Location = new System.Drawing.Point(12, 556);
             this.btnGenerateServerZip.Name = "btnGenerateServerZip";
             this.btnGenerateServerZip.Size = new System.Drawing.Size(461, 23);
             this.btnGenerateServerZip.TabIndex = 3;
@@ -75,22 +66,40 @@
             // 
             // txtLog
             // 
-            this.txtLog.Location = new System.Drawing.Point(12, 357);
+            this.txtLog.Location = new System.Drawing.Point(12, 399);
             this.txtLog.Multiline = true;
             this.txtLog.Name = "txtLog";
             this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
             this.txtLog.Size = new System.Drawing.Size(461, 151);
             this.txtLog.TabIndex = 4;
             // 
+            // cmbGameVersions
+            // 
+            this.cmbGameVersions.FormattingEnabled = true;
+            this.cmbGameVersions.Location = new System.Drawing.Point(12, 41);
+            this.cmbGameVersions.Name = "cmbGameVersions";
+            this.cmbGameVersions.Size = new System.Drawing.Size(220, 23);
+            this.cmbGameVersions.TabIndex = 5;
+            this.cmbGameVersions.SelectedIndexChanged += new System.EventHandler(this.cmbGameVersions_SelectedIndexChanged);
+            // 
+            // cmbForgeVersion
+            // 
+            this.cmbForgeVersion.FormattingEnabled = true;
+            this.cmbForgeVersion.Location = new System.Drawing.Point(248, 41);
+            this.cmbForgeVersion.Name = "cmbForgeVersion";
+            this.cmbForgeVersion.Size = new System.Drawing.Size(225, 23);
+            this.cmbForgeVersion.TabIndex = 6;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(485, 549);
+            this.ClientSize = new System.Drawing.Size(485, 591);
+            this.Controls.Add(this.cmbForgeVersion);
+            this.Controls.Add(this.cmbGameVersions);
             this.Controls.Add(this.txtLog);
             this.Controls.Add(this.btnGenerateServerZip);
             this.Controls.Add(this.chkFolders);
-            this.Controls.Add(this.btnSelectForgeServerFolder);
             this.Controls.Add(this.btnSelectModPackFolder);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
@@ -104,9 +113,10 @@
         #endregion
 
         private Button btnSelectModPackFolder;
-        private Button btnSelectForgeServerFolder;
         private CheckedListBox chkFolders;
         private Button btnGenerateServerZip;
         private TextBox txtLog;
+        private ComboBox cmbGameVersions;
+        private ComboBox cmbForgeVersion;
     }
 }
--- MainForm.cs
+++ MainForm.cs
@@ -1,189 +1,222 @@
+using Newtonsoft.Json.Linq;
 using System.IO.Compression;
+using System.Net;
+using System.Net.Http;
 
-namespace ServerGenerator
+namespace ServerGenerator;
+
+public partial class MainForm : Form
 {
-    public partial class MainForm : Form
+
+    dynamic json;
+    public MainForm()
     {
-        public MainForm()
+        InitializeComponent();
+        LoadForgeVersions();
+    }
+
+    public async void LoadForgeVersions()
+    {
+        HttpClient client = new();
+        string result = await client.GetStringAsync("https://meta.modrinth.com/gamedata/forge/v0/manifest.json");
+        json = JObject.Parse(result);
+
+        foreach (JObject gameVersion in json.gameVersions)
         {
-            InitializeComponent();
+            string? value = gameVersion["id"].Value<string>();
+            cmbGameVersions.Items.Add(value);
         }
+    }
 
-        var cfApiClient = new CurseForge.APIClient.ApiClient(apiKey, partnerId, contactEmail);
+    private void cmbGameVersions_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        cmbForgeVersion.Items.Clear();
+        foreach (JObject gameVersion in json.gameVersions)
+        {
+            string? id = gameVersion["id"].Value<string>();
+            if (id == cmbGameVersions.SelectedItem.ToString())
+            {
+                JArray? value = gameVersion["loaders"].Value<JArray>();
+                foreach (var item in value)
+                {
+                    cmbForgeVersion.Items.Add(item["id"].Value<string>());
+                }
+                return;
+            }
+        }
+    }
 
-        private readonly List<string> commonDirectories = new()
+    private readonly List<string> commonDirectories = new()
         {
             "config", "defaultconfigs", "global_packs", "mods", "scripts"
         };
 
-        private string forgePath, modPackPath;
-        List<string> filesToIgnore = new();
-        private void btnSelectForgeServerFolder_Click(object sender, EventArgs e)
+    private string modPackPath;
+    List<string> filesToIgnore = new();
+    private void btnSelectModPackFolder_Click(object sender, EventArgs e)
+    {
+        FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog()
         {
-            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog()
-            {
-                AutoUpgradeEnabled = true,
-                UseDescriptionForTitle = true,
-                Description = "Select the folder containing the Forge Server"
-            };
+            AutoUpgradeEnabled = true,
+            UseDescriptionForTitle = true,
+            Description = "Select the folder containing the ModPack"
+        };
 
-            if (folderBrowserDialog.ShowDialog() != DialogResult.OK)
-                return;
+        if (folderBrowserDialog.ShowDialog() != DialogResult.OK)
+            return;
 
-            forgePath = folderBrowserDialog.SelectedPath;
+        modPackPath = folderBrowserDialog.SelectedPath;
 
-            txtLog.AppendText($"Forge Folder selected{Environment.NewLine}");
-
+        string[] folders = Directory.GetDirectories(modPackPath);
+        foreach (string folder in folders)
+        {
+            bool chkd = commonDirectories.Any(s => Path.GetFileName(folder).Equals(s));
+            chkFolders.Items.Add(Path.GetFileName(folder), chkd);
         }
 
-        private void btnSelectModPackFolder_Click(object sender, EventArgs e)
+        string[] files = Directory.GetFiles(modPackPath);
+        foreach (string file in files)
         {
-            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog()
+            bool chkd = commonDirectories.Any(s => Path.GetFileName(file).Equals(s));
+            chkFolders.Items.Add(Path.GetFileName(file), chkd);
+
+            if (Path.GetFileName(file).Equals("filesToIgnore.txt"))
             {
-                AutoUpgradeEnabled = true,
-                UseDescriptionForTitle = true,
-                Description = "Select the folder containing the ModPack"
-            };
-
-            if (folderBrowserDialog.ShowDialog() != DialogResult.OK)
-                return;
-
-            modPackPath = folderBrowserDialog.SelectedPath;
-
-            string[] folders = Directory.GetDirectories(modPackPath);
-            foreach (string folder in folders)
-            {
-                bool chkd = commonDirectories.Any(s => Path.GetFileName(folder).Equals(s));
-                chkFolders.Items.Add(Path.GetFileName(folder), chkd);
-            }
-
-            string[] files = Directory.GetFiles(modPackPath);
-            foreach (string file in files)
-            {
-                bool chkd = commonDirectories.Any(s => Path.GetFileName(file).Equals(s));
-                chkFolders.Items.Add(Path.GetFileName(file), chkd);
-
-                if (Path.GetFileName(file).Equals("filesToIgnore.txt"))
-                {
-                    filesToIgnore = new List<string>(File.ReadAllLines(file));
-                }
-            }
-
-            txtLog.AppendText($"ModPack Folder selected. Common modpacks folders have been already checked{Environment.NewLine}");
-            if (filesToIgnore.Count > 0)
-            {
-                foreach (string fileToIgnore in filesToIgnore)
-                {
-                    txtLog.AppendText($"Files with this pattern will be ignored: {fileToIgnore}{Environment.NewLine}");
-                }
+                filesToIgnore = new List<string>(File.ReadAllLines(file));
             }
         }
 
-        private void btnGenerateServerZip_Click(object sender, EventArgs e)
+        txtLog.AppendText($"ModPack Folder selected. Common modpacks folders have been already checked{Environment.NewLine}");
+        if (filesToIgnore.Count > 0)
         {
-            if (string.IsNullOrEmpty(forgePath))
+            foreach (string fileToIgnore in filesToIgnore)
             {
-                MessageBox.Show("No Forge Folder selected.");
-                return;
+                txtLog.AppendText($"Files with this pattern will be ignored: {fileToIgnore}{Environment.NewLine}");
             }
-            if (string.IsNullOrEmpty(modPackPath))
-            {
-                MessageBox.Show("No ModPack selected.");
-                return;
-            }
-            this.Cursor = Cursors.WaitCursor;
+        }
+    }
 
-            string tmpPath = Path.Combine(forgePath, "../tmp");
-            SaveFileDialog saveFileDialog = new SaveFileDialog()
-            {
-                InitialDirectory = Path.Combine(forgePath, ".."),
-                Filter = "Zip File|*.zip"
-            };
-            if (saveFileDialog.ShowDialog() != DialogResult.OK)
-            {
-                txtLog.AppendText($"Generation cancelled{Environment.NewLine}");
-                this.Cursor = Cursors.Default;
-                return;
-            }
-            Directory.CreateDirectory(tmpPath);
-            Copy(forgePath, tmpPath);
-            foreach (var item in chkFolders.CheckedItems)
-            {
-                CopyIgnoring(Path.Combine(modPackPath, item.ToString()), Path.Combine(tmpPath, item.ToString()), filesToIgnore);
-            }
-            
+    private void btnGenerateServerZip_Click(object sender, EventArgs e)
+    {
+        if (cmbGameVersions.SelectedItem == null)
+        {
+            MessageBox.Show("No Forge version selected.");
+            return;
+        }
+        if (string.IsNullOrEmpty(modPackPath))
+        {
+            MessageBox.Show("No ModPack selected.");
+            return;
+        }
+        this.Cursor = Cursors.WaitCursor;
+
+        string tmpPath = Path.Combine(modPackPath, "../tmp");
+        SaveFileDialog saveFileDialog = new SaveFileDialog()
+        {
+            InitialDirectory = Path.Combine(modPackPath, ".."),
+            Filter = "Zip File|*.zip"
+        };
+        if (saveFileDialog.ShowDialog() != DialogResult.OK)
+        {
+            txtLog.AppendText($"Generation cancelled{Environment.NewLine}");
+            this.Cursor = Cursors.Default;
+            return;
+        }
+        Directory.CreateDirectory(tmpPath);
+        foreach (var item in chkFolders.CheckedItems)
+        {
+            CopyIgnoring(Path.Combine(modPackPath, item.ToString()), Path.Combine(tmpPath, item.ToString()), filesToIgnore);
+        }
+
+        Task t = DownloadForgeInstaller(tmpPath);
+        t.ContinueWith(task =>
+        {
             ZipFile.CreateFromDirectory(tmpPath, saveFileDialog.FileName);
             Directory.Delete(tmpPath, true);
             txtLog.AppendText($"Zip created{Environment.NewLine}");
             this.Cursor = Cursors.Default;
+        }, TaskScheduler.FromCurrentSynchronizationContext());
+    }
+
+    private async Task DownloadForgeInstaller(string path)
+    {
+        HttpClient client = new();
+        var httpResult = await client.GetAsync($"https://maven.minecraftforge.net/net/minecraftforge/forge/{cmbForgeVersion.SelectedItem}/forge-{cmbForgeVersion.SelectedItem}-installer.jar");
+        if (!httpResult.IsSuccessStatusCode)
+        {
+            txtLog.AppendText($"Failed to download Forge installer{Environment.NewLine}");
+            return;
+        }
+        using var resultStream = await httpResult.Content.ReadAsStreamAsync();
+        using var fileStream = File.Create(Path.Combine(path, $"forge-{cmbForgeVersion.SelectedItem}-installer.jar"));
+        resultStream.CopyTo(fileStream);
+    }
+
+    //TY Stackoverflow
+    public void Copy(string sourceDirectory, string targetDirectory)
+    {
+        DirectoryInfo diSource = new DirectoryInfo(sourceDirectory);
+        DirectoryInfo diTarget = new DirectoryInfo(targetDirectory);
+
+        CopyAll(diSource, diTarget);
+    }
+
+    public void CopyAll(DirectoryInfo source, DirectoryInfo target)
+    {
+        Directory.CreateDirectory(target.FullName);
+
+        // Copy each file into the new directory.
+        foreach (FileInfo fi in source.GetFiles())
+        {
+            Console.WriteLine(@"Copying {0}\{1}", target.FullName, fi.Name);
+            fi.CopyTo(Path.Combine(target.FullName, fi.Name), true);
         }
 
-        //TY Stackoverflow
-        public void Copy(string sourceDirectory, string targetDirectory)
+        // Copy each subdirectory using recursion.
+        foreach (DirectoryInfo diSourceSubDir in source.GetDirectories())
         {
-            DirectoryInfo diSource = new DirectoryInfo(sourceDirectory);
-            DirectoryInfo diTarget = new DirectoryInfo(targetDirectory);
+            DirectoryInfo nextTargetSubDir = target.CreateSubdirectory(diSourceSubDir.Name);
+            CopyAll(diSourceSubDir, nextTargetSubDir);
+        }
+    }
 
-            CopyAll(diSource, diTarget);
+    public void CopyIgnoring(string sourceDirectory, string targetDirectory, List<string> toIgnore)
+    {
+        DirectoryInfo diSource = new DirectoryInfo(sourceDirectory);
+        DirectoryInfo diTarget = new DirectoryInfo(targetDirectory);
+
+        CopyAllIgnoring(diSource, diTarget, toIgnore);
+    }
+
+    public void CopyAllIgnoring(DirectoryInfo source, DirectoryInfo target, List<string> toIgnore)
+    {
+        List<FileInfo> fileInfoToIgnore = new List<FileInfo>();
+        foreach (string patternToIgnore in toIgnore)
+        {
+            foreach (FileInfo f in source.GetFiles(patternToIgnore))
+            {
+                fileInfoToIgnore.Add(f);
+            }
         }
 
-        public void CopyAll(DirectoryInfo source, DirectoryInfo target)
+        Directory.CreateDirectory(target.FullName);
+        // Copy each file into the new directory.
+        foreach (FileInfo fi in source.GetFiles())
         {
-            Directory.CreateDirectory(target.FullName);
-
-            // Copy each file into the new directory.
-            foreach (FileInfo fi in source.GetFiles())
+            if (fileInfoToIgnore.Any(fileInfo => fi.FullName.Equals(fileInfo.FullName)))
             {
-                Console.WriteLine(@"Copying {0}\{1}", target.FullName, fi.Name);
-                fi.CopyTo(Path.Combine(target.FullName, fi.Name), true);
+                txtLog.AppendText($"File '{fi.Name}' ignored{Environment.NewLine}");
+                continue;
             }
-
-            // Copy each subdirectory using recursion.
-            foreach (DirectoryInfo diSourceSubDir in source.GetDirectories())
-            {
-                DirectoryInfo nextTargetSubDir = target.CreateSubdirectory(diSourceSubDir.Name);
-                CopyAll(diSourceSubDir, nextTargetSubDir);
-            }
+            Console.WriteLine(@"Copying {0}\{1}", target.FullName, fi.Name);
+            fi.CopyTo(Path.Combine(target.FullName, fi.Name), true);
         }
 
-        public void CopyIgnoring(string sourceDirectory, string targetDirectory, List<string> toIgnore)
+        // Copy each subdirectory using recursion.
+        foreach (DirectoryInfo diSourceSubDir in source.GetDirectories())
         {
-            DirectoryInfo diSource = new DirectoryInfo(sourceDirectory);
-            DirectoryInfo diTarget = new DirectoryInfo(targetDirectory);
-
-            CopyAllIgnoring(diSource, diTarget, toIgnore);
-        }
-
-        public void CopyAllIgnoring(DirectoryInfo source, DirectoryInfo target, List<string> toIgnore)
-        {
-            List<FileInfo> fileInfoToIgnore = new List<FileInfo>();
-            foreach (string patternToIgnore in toIgnore)
-            {
-                foreach (FileInfo f in source.GetFiles(patternToIgnore))
-                {
-                    fileInfoToIgnore.Add(f);
-                }
-            }
-
-            Directory.CreateDirectory(target.FullName);
-            // Copy each file into the new directory.
-            foreach (FileInfo fi in source.GetFiles())
-            {
-                if (fileInfoToIgnore.Any(fileInfo => fi.FullName.Equals(fileInfo.FullName)))
-                {
-                    txtLog.AppendText($"File '{fi.Name}' ignored{Environment.NewLine}");
-                    continue;
-                }
-                Console.WriteLine(@"Copying {0}\{1}", target.FullName, fi.Name);
-                fi.CopyTo(Path.Combine(target.FullName, fi.Name), true);
-            }
-
-            // Copy each subdirectory using recursion.
-            foreach (DirectoryInfo diSourceSubDir in source.GetDirectories())
-            {
-                DirectoryInfo nextTargetSubDir = target.CreateSubdirectory(diSourceSubDir.Name);
-                CopyAllIgnoring(diSourceSubDir, nextTargetSubDir, toIgnore);
-            }
+            DirectoryInfo nextTargetSubDir = target.CreateSubdirectory(diSourceSubDir.Name);
+            CopyAllIgnoring(diSourceSubDir, nextTargetSubDir, toIgnore);
         }
     }
 }
