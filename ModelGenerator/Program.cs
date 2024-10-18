6e879adfa4d4c4a414a682a107e8f767307253fa
Insane96 <delvillano.alberto@gmail.com>
2024-10-18 16:08:08 +0200

    modelgenerator

---
+++ ModelGenerator/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Path: ");
+string? path = Console.ReadLine();
+
+if (string.IsNullOrEmpty(path) || !File.Exists(path))
+{
+    Console.WriteLine("Failed to read path");
+    return;
+}
+
+Console.WriteLine("Folder path: " + path);
+string json = File.ReadAllText(path);
+
+for (int i = 1; i <= 16; i++)
+{
+    string jsonPath = Path.Combine(Path.GetDirectoryName(path), $"scute_{i}.json");
+    string newJson = json
+        .Replace("#1#", (i - 1).ToString())
+        .Replace("#2#", (i).ToString());
+
+    Console.WriteLine(newJson);
+    //File.WriteAllText(jsonPath, newJson);
+}
